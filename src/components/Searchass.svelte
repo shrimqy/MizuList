b0f1fa12fdcc810de2d1d104ea6c76dbe223e56c
Arjun O Nair <arjunonair123@gmail.com>
2023-04-29 17:01:44 +0000

    SearchBar Changes

---
+++ src/components/Searchass.svelte
@@ -0,0 +1,10 @@
+<script>
+    import SearchBar from './SearchBar.svelte';
+  
+    const handleSearch = (items) => {
+      console.log(items);
+    };
+  </script>
+  
+  <SearchBar onSubmit={handleSearch} />
+  
