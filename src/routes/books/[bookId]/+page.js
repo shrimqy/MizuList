c8e0aec4b8c38c54b07969cd080581c38e909251
shrimpyvongue <shrimpyvongue@gmail.com>
2023-05-16 22:36:12 +0530

    Added search API to fetch more details on the book

--- src/routes/books/[bookId]/+page.js
+++ src/routes/books/[bookId]/+page.js
@@ -1,16 +1,26 @@
-export const load = ({ fetch, params }) => {
+export const load = async ({ fetch, params }) => {
     const fetchBook = async (id) => {
-        const workres = await fetch(`https://openlibrary.org/works/${id}.json`);
-        const work = await workres.json();
-        const bookres = await fetch(`https://openlibrary.org/search.json?title=${work.title}&limit=18`)
-        const book = await bookres.json();
-        console.log(book);
-        
-        return work
-    }
+      const workres = await fetch(`https://openlibrary.org/works/${id}.json`);
+      const work = await workres.json();
+  
+      const bookres = await fetch(
+        `https://openlibrary.org/search.json?title=${work.title}&limit=18`
+      );
+      const bookData = await bookres.json();
+  
+      const matchingBooks = bookData.docs.filter((book) => book.key === work.key); // filter callback checks if the key property of each book matches the work.key value
+  
+      return {
+        work,
+        matchingBooks,
+      };
+    };
+  
+    const result = await fetchBook(params.bookId);
+  
     return {
-        book: fetchBook(params.bookId)
-    }
-}
-
-
+      work: result.work,
+      bookData: result.matchingBooks[0],
+    };
+  };
+  
