30ae314d24dbd0cca3cb3885543cbeeff3c4d24d
shrimpyvongue <shrimpyvongue@gmail.com>
2024-06-12 00:27:38 +0530

    Prisma Vercel Fix

--- svelte.config.js
+++ svelte.config.js
@@ -17,9 +17,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x',
-    }),
+    adapter: adapter(),
   },
 };
  
